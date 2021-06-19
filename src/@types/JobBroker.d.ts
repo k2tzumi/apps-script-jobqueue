3b2509e70f8e9b6b0bf98d1d095b2d671a0f7d84
k2tzumi <katzumi+github@gmail.com>
2021-06-19 22:04:36 +0900

    📝 Fix

--- src/@types/JobBroker.d.ts
+++ src/@types/JobBroker.d.ts
@@ -1,18 +1,31 @@
-declare module JobBroker {
-    interface JobBroker {
-        enqueueAsyncJob(callback: JobFunction, parameter: Record<string, any>): void;
-        consumeAsyncJob(closure: JobFunction, handler: string | null): void;
-        createDelaydJob(scheduled_at: Date): DelayedJobBroker;
+declare module "apps-script-jobqueue" {
+    export function enqueueAsyncJob(callback: JobFunction, parameter: Record<string, any>): void;
+    export function consumeAsyncJob(closure: JobFunction, handler: string | null): void;
+    export function createDelaydJob(scheduled_at: Date): DelayedJobBroker;
+    export function perform(closure: JobFunction): void;
+
+    export interface JobBroker {
+        enqueue(callback: JobFunction, parameter: Record<string, any>): void;
+        dequeue(handler: string): Job | null;
         perform(closure: JobFunction): void;
+        consumeJob(closure: JobFunction, handler: string | null): void;
     }
 
-    interface DelayedJobBroker {
-        performLater(callback: JobFunction, arameter: Record<string, any>): void;
+    export interface DelayedJobBroker extends JobBroker {
+        performLater(callback: JobFunction,parameter: Record<string, any>): void
     }
 
-    type JobFunction = (parameter: Record<string, any>) => void;
-}
+    export interface JobParameter {
+        id: string;
+        state: string;
+        scheduled_at?: number;
+        start_at?: number;
+        end_at?: number;
+        created_at: number;
+        handler: string;
+        parameter: string;
+    }
 
-declare module "k2tzumi/apps-script-jobqueue" {
-    export = JobBroker;
-}
+    export type Trigger = GoogleAppsScript.Script.Trigger;
+    export type JobFunction = (parameter: Record<string, any>) => void;
+    export type Job = { parameter: JobParameter; trigger: Trigger }
